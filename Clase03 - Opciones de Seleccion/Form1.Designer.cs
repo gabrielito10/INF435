9a78a56cf1684702769c89cf56f41c87a38fa871
Gabriel Ibañez <j.gabito@gmail.com>
2020-12-15 11:09:48 -0400

    INF421

--- Clase03 - Opciones de Seleccion/Form1.Designer.cs
+++ Clase03 - Opciones de Seleccion/Form1.Designer.cs
@@ -28,113 +28,177 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.radioButton4 = new System.Windows.Forms.RadioButton();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
-            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.opSuma = new System.Windows.Forms.RadioButton();
+            this.opRojo = new System.Windows.Forms.CheckBox();
+            this.opResta = new System.Windows.Forms.RadioButton();
+            this.opMultiplicacion = new System.Windows.Forms.RadioButton();
+            this.opDivision = new System.Windows.Forms.RadioButton();
+            this.opAmarillo = new System.Windows.Forms.CheckBox();
+            this.opVerde = new System.Windows.Forms.CheckBox();
+            this.opAzul = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnMostrarColores = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // radioButton1
+            // opSuma
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(68, 39);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(85, 17);
-            this.radioButton1.TabIndex = 0;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "radioButton1";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.opSuma.AutoSize = true;
+            this.opSuma.Location = new System.Drawing.Point(53, 84);
+            this.opSuma.Name = "opSuma";
+            this.opSuma.Size = new System.Drawing.Size(56, 17);
+            this.opSuma.TabIndex = 0;
+            this.opSuma.TabStop = true;
+            this.opSuma.Text = "SUMA";
+            this.opSuma.UseVisualStyleBackColor = true;
             // 
-            // checkBox1
+            // opRojo
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(68, 147);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 17);
-            this.checkBox1.TabIndex = 1;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.opRojo.AutoSize = true;
+            this.opRojo.Location = new System.Drawing.Point(353, 39);
+            this.opRojo.Name = "opRojo";
+            this.opRojo.Size = new System.Drawing.Size(74, 17);
+            this.opRojo.TabIndex = 1;
+            this.opRojo.Text = "CELESTE";
+            this.opRojo.UseVisualStyleBackColor = true;
             // 
-            // radioButton2
+            // opResta
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(68, 63);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(85, 17);
-            this.radioButton2.TabIndex = 2;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "radioButton2";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.opResta.AutoSize = true;
+            this.opResta.Location = new System.Drawing.Point(53, 108);
+            this.opResta.Name = "opResta";
+            this.opResta.Size = new System.Drawing.Size(61, 17);
+            this.opResta.TabIndex = 2;
+            this.opResta.TabStop = true;
+            this.opResta.Text = "RESTA";
+            this.opResta.UseVisualStyleBackColor = true;
             // 
-            // radioButton3
+            // opMultiplicacion
             // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(68, 86);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(85, 17);
-            this.radioButton3.TabIndex = 3;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "radioButton3";
-            this.radioButton3.UseVisualStyleBackColor = true;
+            this.opMultiplicacion.AutoSize = true;
+            this.opMultiplicacion.Location = new System.Drawing.Point(53, 131);
+            this.opMultiplicacion.Name = "opMultiplicacion";
+            this.opMultiplicacion.Size = new System.Drawing.Size(114, 17);
+            this.opMultiplicacion.TabIndex = 3;
+            this.opMultiplicacion.TabStop = true;
+            this.opMultiplicacion.Text = "MULTIPLICACION";
+            this.opMultiplicacion.UseVisualStyleBackColor = true;
             // 
-            // radioButton4
+            // opDivision
             // 
-            this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(68, 109);
-            this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(85, 17);
-            this.radioButton4.TabIndex = 4;
-            this.radioButton4.TabStop = true;
-            this.radioButton4.Text = "radioButton4";
-            this.radioButton4.UseVisualStyleBackColor = true;
+            this.opDivision.AutoSize = true;
+            this.opDivision.Location = new System.Drawing.Point(53, 154);
+            this.opDivision.Name = "opDivision";
+            this.opDivision.Size = new System.Drawing.Size(72, 17);
+            this.opDivision.TabIndex = 4;
+            this.opDivision.TabStop = true;
+            this.opDivision.Text = "DIVISION";
+            this.opDivision.UseVisualStyleBackColor = true;
             // 
-            // checkBox2
+            // opAmarillo
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(68, 170);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(80, 17);
-            this.checkBox2.TabIndex = 5;
-            this.checkBox2.Text = "checkBox2";
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.opAmarillo.AutoSize = true;
+            this.opAmarillo.Location = new System.Drawing.Point(353, 62);
+            this.opAmarillo.Name = "opAmarillo";
+            this.opAmarillo.Size = new System.Drawing.Size(80, 17);
+            this.opAmarillo.TabIndex = 5;
+            this.opAmarillo.Text = "AMARILLO";
+            this.opAmarillo.UseVisualStyleBackColor = true;
             // 
-            // checkBox3
+            // opVerde
             // 
-            this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(68, 193);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(80, 17);
-            this.checkBox3.TabIndex = 6;
-            this.checkBox3.Text = "checkBox3";
-            this.checkBox3.UseVisualStyleBackColor = true;
+            this.opVerde.AutoSize = true;
+            this.opVerde.Location = new System.Drawing.Point(353, 85);
+            this.opVerde.Name = "opVerde";
+            this.opVerde.Size = new System.Drawing.Size(63, 17);
+            this.opVerde.TabIndex = 6;
+            this.opVerde.Text = "VERDE";
+            this.opVerde.UseVisualStyleBackColor = true;
             // 
-            // checkBox4
+            // opAzul
             // 
-            this.checkBox4.AutoSize = true;
-            this.checkBox4.Location = new System.Drawing.Point(68, 216);
-            this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(80, 17);
-            this.checkBox4.TabIndex = 7;
-            this.checkBox4.Text = "checkBox4";
-            this.checkBox4.UseVisualStyleBackColor = true;
+            this.opAzul.AutoSize = true;
+            this.opAzul.Location = new System.Drawing.Point(353, 108);
+            this.opAzul.Name = "opAzul";
+            this.opAzul.Size = new System.Drawing.Size(54, 17);
+            this.opAzul.TabIndex = 7;
+            this.opAzul.Text = "AZUL";
+            this.opAzul.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "NUMERO 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "NUMERO 2";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(91, 10);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(73, 20);
+            this.txtNum1.TabIndex = 10;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(91, 39);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(73, 20);
+            this.txtNum2.TabIndex = 11;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(40, 193);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(111, 38);
+            this.btnCalcular.TabIndex = 12;
+            this.btnCalcular.Text = "CALCULAR";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnMostrarColores
+            // 
+            this.btnMostrarColores.Location = new System.Drawing.Point(332, 177);
+            this.btnMostrarColores.Name = "btnMostrarColores";
+            this.btnMostrarColores.Size = new System.Drawing.Size(137, 54);
+            this.btnMostrarColores.TabIndex = 13;
+            this.btnMostrarColores.Text = "MOSTRAR COLORES";
+            this.btnMostrarColores.UseVisualStyleBackColor = true;
+            this.btnMostrarColores.Click += new System.EventHandler(this.btnMostrarColores_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(522, 287);
-            this.Controls.Add(this.checkBox4);
-            this.Controls.Add(this.checkBox3);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.radioButton4);
-            this.Controls.Add(this.radioButton3);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.btnMostrarColores);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.opAzul);
+            this.Controls.Add(this.opVerde);
+            this.Controls.Add(this.opAmarillo);
+            this.Controls.Add(this.opDivision);
+            this.Controls.Add(this.opMultiplicacion);
+            this.Controls.Add(this.opResta);
+            this.Controls.Add(this.opRojo);
+            this.Controls.Add(this.opSuma);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
@@ -145,14 +209,20 @@
 
         #endregion
 
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton3;
-        private System.Windows.Forms.RadioButton radioButton4;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.CheckBox checkBox3;
-        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.RadioButton opSuma;
+        private System.Windows.Forms.CheckBox opRojo;
+        private System.Windows.Forms.RadioButton opResta;
+        private System.Windows.Forms.RadioButton opMultiplicacion;
+        private System.Windows.Forms.RadioButton opDivision;
+        private System.Windows.Forms.CheckBox opAmarillo;
+        private System.Windows.Forms.CheckBox opVerde;
+        private System.Windows.Forms.CheckBox opAzul;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnMostrarColores;
     }
 }
 
