3351ca8a85bad0a681537b15189826b0f0ab37ab
Gabriel Ibañez <j.gabito.ic@gmail.com>
2021-04-19 17:13:24 -0400

    INF435

---
+++ 03RadioButton y CheckBox/03RadioButton y CheckBox/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace _03RadioButton_y_CheckBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.opSoltero = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.opCasado = new System.Windows.Forms.RadioButton();
+            this.opDivorciado = new System.Windows.Forms.RadioButton();
+            this.opViudo = new System.Windows.Forms.RadioButton();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.btnEstadoCivil = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // opSoltero
+            // 
+            this.opSoltero.AutoSize = true;
+            this.opSoltero.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.opSoltero.Location = new System.Drawing.Point(58, 65);
+            this.opSoltero.Name = "opSoltero";
+            this.opSoltero.Size = new System.Drawing.Size(85, 24);
+            this.opSoltero.TabIndex = 0;
+            this.opSoltero.Text = "Soltero";
+            this.opSoltero.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(234, 65);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(115, 24);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // opCasado
+            // 
+            this.opCasado.AutoSize = true;
+            this.opCasado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.opCasado.Location = new System.Drawing.Point(58, 105);
+            this.opCasado.Name = "opCasado";
+            this.opCasado.Size = new System.Drawing.Size(88, 24);
+            this.opCasado.TabIndex = 2;
+            this.opCasado.Text = "Casado";
+            this.opCasado.UseVisualStyleBackColor = true;
+            // 
+            // opDivorciado
+            // 
+            this.opDivorciado.AutoSize = true;
+            this.opDivorciado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.opDivorciado.Location = new System.Drawing.Point(58, 146);
+            this.opDivorciado.Name = "opDivorciado";
+            this.opDivorciado.Size = new System.Drawing.Size(111, 24);
+            this.opDivorciado.TabIndex = 3;
+            this.opDivorciado.TabStop = true;
+            this.opDivorciado.Text = "Divorciado";
+            this.opDivorciado.UseVisualStyleBackColor = true;
+            // 
+            // opViudo
+            // 
+            this.opViudo.AutoSize = true;
+            this.opViudo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.opViudo.Location = new System.Drawing.Point(58, 185);
+            this.opViudo.Name = "opViudo";
+            this.opViudo.Size = new System.Drawing.Size(73, 24);
+            this.opViudo.TabIndex = 4;
+            this.opViudo.TabStop = true;
+            this.opViudo.Text = "Viudo";
+            this.opViudo.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.Location = new System.Drawing.Point(234, 106);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(115, 24);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox3.Location = new System.Drawing.Point(234, 147);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(115, 24);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox4.Location = new System.Drawing.Point(234, 185);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(115, 24);
+            this.checkBox4.TabIndex = 7;
+            this.checkBox4.Text = "checkBox4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // btnEstadoCivil
+            // 
+            this.btnEstadoCivil.Location = new System.Drawing.Point(58, 236);
+            this.btnEstadoCivil.Name = "btnEstadoCivil";
+            this.btnEstadoCivil.Size = new System.Drawing.Size(111, 45);
+            this.btnEstadoCivil.TabIndex = 8;
+            this.btnEstadoCivil.Text = "Mostrar";
+            this.btnEstadoCivil.UseVisualStyleBackColor = true;
+            this.btnEstadoCivil.Click += new System.EventHandler(this.btnEstadoCivil_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 293);
+            this.Controls.Add(this.btnEstadoCivil);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.opViudo);
+            this.Controls.Add(this.opDivorciado);
+            this.Controls.Add(this.opCasado);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.opSoltero);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton opSoltero;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RadioButton opCasado;
+        private System.Windows.Forms.RadioButton opDivorciado;
+        private System.Windows.Forms.RadioButton opViudo;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Button btnEstadoCivil;
+    }
+}
+
---
+++ 03RadioButton y CheckBox/03RadioButton y CheckBox/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _03RadioButton_y_CheckBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEstadoCivil_Click(object sender, EventArgs e)
+        {
+            if (opSoltero.Checked == true)
+                MessageBox.Show("Ud. Es Soltero");
+            if (opCasado.Checked == true)
+                MessageBox.Show("Ud. Casado");
+            if (opDivorciado.Checked)
+                MessageBox.Show("Ud. es divorciado");
+            if (opViudo.Checked)
+                MessageBox.Show("Ud. es viudo");
+        }
+    }
+}
