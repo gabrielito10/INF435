6528f7facae28ec70ede5ee76491b3d2bba15a02
Gabriel Ibañez <j.gabito.ic@gmail.com>
2021-05-03 17:46:10 -0400

    INF435

---
+++ 08RichTextBox/08RichTextBox/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace _08RichTextBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLetras = new System.Windows.Forms.RichTextBox();
+            this.btnAplicar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtLetras
+            // 
+            this.txtLetras.Location = new System.Drawing.Point(12, 22);
+            this.txtLetras.Name = "txtLetras";
+            this.txtLetras.Size = new System.Drawing.Size(411, 180);
+            this.txtLetras.TabIndex = 1;
+            this.txtLetras.Text = "INGENIERIA INFORMATICA\nFACULTAD CIENCIAS PURAS\nUNIVERSIDAD TOMAS FRIAS\n";
+            // 
+            // btnAplicar
+            // 
+            this.btnAplicar.Location = new System.Drawing.Point(150, 227);
+            this.btnAplicar.Name = "btnAplicar";
+            this.btnAplicar.Size = new System.Drawing.Size(158, 23);
+            this.btnAplicar.TabIndex = 2;
+            this.btnAplicar.Text = "APLICAR CAMBIOS";
+            this.btnAplicar.UseVisualStyleBackColor = true;
+            this.btnAplicar.Click += new System.EventHandler(this.btnAplicar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 262);
+            this.Controls.Add(this.btnAplicar);
+            this.Controls.Add(this.txtLetras);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtLetras;
+        private System.Windows.Forms.Button btnAplicar;
+    }
+}
+
---
+++ 08RichTextBox/08RichTextBox/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _08RichTextBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAplicar_Click(object sender, EventArgs e)
+        {
+            txtLetras.Select(5, 15);
+            txtLetras.SelectionBackColor = Color.Red;
+            txtLetras.SelectionFont = new Font("Impact",20,FontStyle.Bold);
+            txtLetras.SelectionColor = Color.White;
+
+            //seleecionando una linea
+            int numLinea = 1;
+            int inicio = txtLetras.GetFirstCharIndexFromLine(numLinea);
+            int longitud = txtLetras.Lines[numLinea].Length;
+            txtLetras.Select(inicio, longitud);
+            txtLetras.SelectionColor=Color.Green;
+        }
+    }
+}
