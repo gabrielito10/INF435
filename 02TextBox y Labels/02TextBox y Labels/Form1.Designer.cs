3c11f498c0af207e11ea6eacc79d6ae6e0e970ca
gabrielito10 <j.gabito@gmail.com>
2021-09-06 17:03:46 -0400

    INF435

---
+++ 02TextBox y Labels/02TextBox y Labels/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace _02TextBox_y_Labels
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblValor = new System.Windows.Forms.Label();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.txtN1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtN2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblValor
+            // 
+            this.lblValor.AutoSize = true;
+            this.lblValor.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValor.ForeColor = System.Drawing.Color.Red;
+            this.lblValor.Location = new System.Drawing.Point(42, 47);
+            this.lblValor.Name = "lblValor";
+            this.lblValor.Size = new System.Drawing.Size(138, 19);
+            this.lblValor.TabIndex = 0;
+            this.lblValor.Text = "Digite Valor de N";
+            // 
+            // txtN
+            // 
+            this.txtN.Location = new System.Drawing.Point(195, 46);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(100, 20);
+            this.txtN.TabIndex = 1;
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(130, 85);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(100, 36);
+            this.btnGenerar.TabIndex = 2;
+            this.btnGenerar.Text = "Generar Serie";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // txtN1
+            // 
+            this.txtN1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtN1.Location = new System.Drawing.Point(195, 156);
+            this.txtN1.Name = "txtN1";
+            this.txtN1.Size = new System.Drawing.Size(100, 31);
+            this.txtN1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(42, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Digite Valor de N1";
+            // 
+            // txtN2
+            // 
+            this.txtN2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtN2.Location = new System.Drawing.Point(195, 193);
+            this.txtN2.Name = "txtN2";
+            this.txtN2.Size = new System.Drawing.Size(100, 31);
+            this.txtN2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(42, 194);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Digite Valor de N2";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(130, 238);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(100, 36);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 334);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtN2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtN1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.txtN);
+            this.Controls.Add(this.lblValor);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblValor;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.TextBox txtN1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtN2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
