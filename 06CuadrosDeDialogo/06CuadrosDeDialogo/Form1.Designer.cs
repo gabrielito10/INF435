4b7229fb7380b6ced2de98d9e5b4342709d70ea6
gabrielito10 <j.gabito@gmail.com>
2021-09-28 09:53:14 -0400

    INF435

---
+++ 06CuadrosDeDialogo/06CuadrosDeDialogo/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace _06CuadrosDeDialogo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.miImagen = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.miImagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // miImagen
+            // 
+            this.miImagen.Location = new System.Drawing.Point(54, 12);
+            this.miImagen.Name = "miImagen";
+            this.miImagen.Size = new System.Drawing.Size(415, 250);
+            this.miImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.miImagen.TabIndex = 0;
+            this.miImagen.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(82, 289);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(153, 23);
+            this.btnCargar.TabIndex = 1;
+            this.btnCargar.Text = "CARGAR IMAGEN";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(286, 289);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(146, 23);
+            this.btnGuardar.TabIndex = 2;
+            this.btnGuardar.Text = "GUARDAR IMAGEN";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 330);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.miImagen);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.miImagen)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox miImagen;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.Button btnGuardar;
+    }
+}
+
---
+++ 06CuadrosDeDialogo/06CuadrosDeDialogo/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//AGRAGANDO ENSAMBLADO PARA FORMATO DE IMAGEN
+using System.Drawing.Imaging;
+
+namespace _06CuadrosDeDialogo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            //Directorio de Inicio
+            openFileDialog1.InitialDirectory = @"C:\Users\Gabriel\Pictures";
+            //Nombre del archivo
+            openFileDialog1.FileName = "";
+            //filtrar archivos de tipo imagen BMP, PNG, JPG
+            // Texto|Nombre.Extension1|Texto|Nombre.Extension2
+            openFileDialog1.Filter = "Archivitos BMP|*.BMP|Archivitos JPG|*.JPG"+
+                "|Archivitos PNG|*.PNG";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //cargando imagen dentro del picture box
+                miImagen.Load(openFileDialog1.FileName);
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            //Directorio Inicial
+            saveFileDialog1.InitialDirectory = @"D:\";
+            //Nombre de Archivo
+            saveFileDialog1.FileName = "MiImagen";
+            ImageFormat formato = ImageFormat.Png;
+
+            //filtro
+            saveFileDialog1.Filter= "Archivitos BMP|*.BMP|Archivitos JPG|*.JPG" +
+                "|Archivitos PNG|*.PNG";
+            if (saveFileDialog1.ShowDialog() ==DialogResult.OK)
+            {
+                string extension = System.IO.Path.GetExtension(saveFileDialog1.FileName);
+                switch(extension)
+                {
+                    case ".jpg":
+                        formato = ImageFormat.Jpeg;
+                        break;
+                    case ".png":
+                        formato = ImageFormat.Png;
+                        break;
+                    case ".bmp":
+                        formato = ImageFormat.Bmp;
+                        break;
+
+                }
+                //Descargando la Imagen
+                miImagen.Image.Save(saveFileDialog1.FileName, formato);
+            }
+        }
+    }
+}
