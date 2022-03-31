2b91c16cc0cac981a1ecb581d5bf049d34e43263
gabrielito10 <j.gabito@gmail.com>
2022-03-31 16:44:43 -0400

    INF435

--- 01VentanaEmergente/01VentanaEmergente/Form1.Designer.cs
+++ 01VentanaEmergente/01VentanaEmergente/Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.miBotoncito = new System.Windows.Forms.Button();
+            this.btnDirecto = new System.Windows.Forms.Button();
+            this.btnCaptura = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // miBotoncito
@@ -36,7 +38,7 @@
             this.miBotoncito.BackColor = System.Drawing.Color.Navy;
             this.miBotoncito.Font = new System.Drawing.Font("Viner Hand ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.miBotoncito.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.miBotoncito.Location = new System.Drawing.Point(111, 111);
+            this.miBotoncito.Location = new System.Drawing.Point(22, 38);
             this.miBotoncito.Name = "miBotoncito";
             this.miBotoncito.Size = new System.Drawing.Size(230, 80);
             this.miBotoncito.TabIndex = 0;
@@ -44,13 +46,41 @@
             this.miBotoncito.UseVisualStyleBackColor = false;
             this.miBotoncito.Click += new System.EventHandler(this.miBotoncito_Click);
             // 
+            // btnDirecto
+            // 
+            this.btnDirecto.BackColor = System.Drawing.Color.Navy;
+            this.btnDirecto.Font = new System.Drawing.Font("Viner Hand ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDirecto.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnDirecto.Location = new System.Drawing.Point(289, 38);
+            this.btnDirecto.Name = "btnDirecto";
+            this.btnDirecto.Size = new System.Drawing.Size(230, 80);
+            this.btnDirecto.TabIndex = 1;
+            this.btnDirecto.Text = "Mensaje Directo";
+            this.btnDirecto.UseVisualStyleBackColor = false;
+            this.btnDirecto.Click += new System.EventHandler(this.btnDirecto_Click);
+            // 
+            // btnCaptura
+            // 
+            this.btnCaptura.BackColor = System.Drawing.Color.Navy;
+            this.btnCaptura.Font = new System.Drawing.Font("Viner Hand ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCaptura.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnCaptura.Location = new System.Drawing.Point(168, 164);
+            this.btnCaptura.Name = "btnCaptura";
+            this.btnCaptura.Size = new System.Drawing.Size(230, 80);
+            this.btnCaptura.TabIndex = 2;
+            this.btnCaptura.Text = "Captura de boton";
+            this.btnCaptura.UseVisualStyleBackColor = false;
+            this.btnCaptura.Click += new System.EventHandler(this.btnCaptura_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(145)))), ((int)(((byte)(56)))), ((int)(((byte)(57)))));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(484, 311);
+            this.ClientSize = new System.Drawing.Size(588, 357);
+            this.Controls.Add(this.btnCaptura);
+            this.Controls.Add(this.btnDirecto);
             this.Controls.Add(this.miBotoncito);
             this.DoubleBuffered = true;
             this.Font = new System.Drawing.Font("Tw Cen MT", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -67,6 +97,8 @@
         #endregion
 
         private System.Windows.Forms.Button miBotoncito;
+        private System.Windows.Forms.Button btnDirecto;
+        private System.Windows.Forms.Button btnCaptura;
     }
 }
 
--- 01VentanaEmergente/01VentanaEmergente/Form1.cs
+++ 01VentanaEmergente/01VentanaEmergente/Form1.cs
@@ -19,7 +19,28 @@ namespace _01VentanaEmergente
 
         private void miBotoncito_Click(object sender, EventArgs e)
         {
-            //aqui va al codigo del boton
+            //sacando mensaje con las 4 opciones
+            MessageBox.Show("Desea eliminar este archivo?","Programacion IV",
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+
+            
+            
+        }
+
+        private void btnDirecto_Click(object sender, EventArgs e)
+        {
+            //sacando mensaje directo
+            MessageBox.Show("Este es un mensaje directo");
+        }
+
+        private void btnCaptura_Click(object sender, EventArgs e)
+        {
+            //captpurando el boton
+           if ( MessageBox.Show("Aqui vamos a capturar que boton se presiono", "Programacion IV",
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                MessageBox.Show("Ud presiono Aceptar");
+            }
         }
     }
 }
