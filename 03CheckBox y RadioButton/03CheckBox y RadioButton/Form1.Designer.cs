537c699102df6900a45827bf972a674f08649e81
gabrielito10 <j.gabito@gmail.com>
2022-09-08 16:59:01 -0400

    INF435

---
+++ 03CheckBox y RadioButton/03CheckBox y RadioButton/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace _03CheckBox_y_RadioButton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbtSoltero = new System.Windows.Forms.RadioButton();
+            this.rbtCasado = new System.Windows.Forms.RadioButton();
+            this.rbtDivorciado = new System.Windows.Forms.RadioButton();
+            this.rbtViudo = new System.Windows.Forms.RadioButton();
+            this.btnEstado = new System.Windows.Forms.Button();
+            this.chbFutbol = new System.Windows.Forms.CheckBox();
+            this.chbBasquet = new System.Windows.Forms.CheckBox();
+            this.chbVolibol = new System.Windows.Forms.CheckBox();
+            this.chbAjedrez = new System.Windows.Forms.CheckBox();
+            this.btnDeporte = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rbtSoltero
+            // 
+            this.rbtSoltero.AutoSize = true;
+            this.rbtSoltero.Location = new System.Drawing.Point(61, 34);
+            this.rbtSoltero.Name = "rbtSoltero";
+            this.rbtSoltero.Size = new System.Drawing.Size(76, 17);
+            this.rbtSoltero.TabIndex = 0;
+            this.rbtSoltero.TabStop = true;
+            this.rbtSoltero.Text = "SOLTERO";
+            this.rbtSoltero.UseVisualStyleBackColor = true;
+            // 
+            // rbtCasado
+            // 
+            this.rbtCasado.AutoSize = true;
+            this.rbtCasado.Location = new System.Drawing.Point(61, 57);
+            this.rbtCasado.Name = "rbtCasado";
+            this.rbtCasado.Size = new System.Drawing.Size(69, 17);
+            this.rbtCasado.TabIndex = 1;
+            this.rbtCasado.TabStop = true;
+            this.rbtCasado.Text = "CASADO";
+            this.rbtCasado.UseVisualStyleBackColor = true;
+            // 
+            // rbtDivorciado
+            // 
+            this.rbtDivorciado.AutoSize = true;
+            this.rbtDivorciado.Location = new System.Drawing.Point(61, 80);
+            this.rbtDivorciado.Name = "rbtDivorciado";
+            this.rbtDivorciado.Size = new System.Drawing.Size(92, 17);
+            this.rbtDivorciado.TabIndex = 2;
+            this.rbtDivorciado.TabStop = true;
+            this.rbtDivorciado.Text = "DIVORCIADO";
+            this.rbtDivorciado.UseVisualStyleBackColor = true;
+            // 
+            // rbtViudo
+            // 
+            this.rbtViudo.AutoSize = true;
+            this.rbtViudo.Location = new System.Drawing.Point(61, 103);
+            this.rbtViudo.Name = "rbtViudo";
+            this.rbtViudo.Size = new System.Drawing.Size(59, 17);
+            this.rbtViudo.TabIndex = 3;
+            this.rbtViudo.TabStop = true;
+            this.rbtViudo.Text = "VIUDO";
+            this.rbtViudo.UseVisualStyleBackColor = true;
+            // 
+            // btnEstado
+            // 
+            this.btnEstado.Location = new System.Drawing.Point(37, 150);
+            this.btnEstado.Name = "btnEstado";
+            this.btnEstado.Size = new System.Drawing.Size(134, 32);
+            this.btnEstado.TabIndex = 4;
+            this.btnEstado.Text = "MOSTRAR ESTADO";
+            this.btnEstado.UseVisualStyleBackColor = true;
+            this.btnEstado.Click += new System.EventHandler(this.btnEstado_Click);
+            // 
+            // chbFutbol
+            // 
+            this.chbFutbol.AutoSize = true;
+            this.chbFutbol.Location = new System.Drawing.Point(292, 34);
+            this.chbFutbol.Name = "chbFutbol";
+            this.chbFutbol.Size = new System.Drawing.Size(68, 17);
+            this.chbFutbol.TabIndex = 5;
+            this.chbFutbol.Text = "FUTBOL";
+            this.chbFutbol.UseVisualStyleBackColor = true;
+            // 
+            // chbBasquet
+            // 
+            this.chbBasquet.AutoSize = true;
+            this.chbBasquet.Location = new System.Drawing.Point(292, 58);
+            this.chbBasquet.Name = "chbBasquet";
+            this.chbBasquet.Size = new System.Drawing.Size(77, 17);
+            this.chbBasquet.TabIndex = 6;
+            this.chbBasquet.Text = "BASQUET";
+            this.chbBasquet.UseVisualStyleBackColor = true;
+            // 
+            // chbVolibol
+            // 
+            this.chbVolibol.AutoSize = true;
+            this.chbVolibol.Location = new System.Drawing.Point(292, 81);
+            this.chbVolibol.Name = "chbVolibol";
+            this.chbVolibol.Size = new System.Drawing.Size(71, 17);
+            this.chbVolibol.TabIndex = 7;
+            this.chbVolibol.Text = "VOLIBOL";
+            this.chbVolibol.UseVisualStyleBackColor = true;
+            // 
+            // chbAjedrez
+            // 
+            this.chbAjedrez.AutoSize = true;
+            this.chbAjedrez.Location = new System.Drawing.Point(292, 104);
+            this.chbAjedrez.Name = "chbAjedrez";
+            this.chbAjedrez.Size = new System.Drawing.Size(75, 17);
+            this.chbAjedrez.TabIndex = 8;
+            this.chbAjedrez.Text = "AJEDREZ";
+            this.chbAjedrez.UseVisualStyleBackColor = true;
+            // 
+            // btnDeporte
+            // 
+            this.btnDeporte.Location = new System.Drawing.Point(272, 150);
+            this.btnDeporte.Name = "btnDeporte";
+            this.btnDeporte.Size = new System.Drawing.Size(134, 32);
+            this.btnDeporte.TabIndex = 9;
+            this.btnDeporte.Text = "MOSTRAR DEPORTE";
+            this.btnDeporte.UseVisualStyleBackColor = true;
+            this.btnDeporte.Click += new System.EventHandler(this.btnDeporte_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 225);
+            this.Controls.Add(this.btnDeporte);
+            this.Controls.Add(this.chbAjedrez);
+            this.Controls.Add(this.chbVolibol);
+            this.Controls.Add(this.chbBasquet);
+            this.Controls.Add(this.chbFutbol);
+            this.Controls.Add(this.btnEstado);
+            this.Controls.Add(this.rbtViudo);
+            this.Controls.Add(this.rbtDivorciado);
+            this.Controls.Add(this.rbtCasado);
+            this.Controls.Add(this.rbtSoltero);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbtSoltero;
+        private System.Windows.Forms.RadioButton rbtCasado;
+        private System.Windows.Forms.RadioButton rbtDivorciado;
+        private System.Windows.Forms.RadioButton rbtViudo;
+        private System.Windows.Forms.Button btnEstado;
+        private System.Windows.Forms.CheckBox chbFutbol;
+        private System.Windows.Forms.CheckBox chbBasquet;
+        private System.Windows.Forms.CheckBox chbVolibol;
+        private System.Windows.Forms.CheckBox chbAjedrez;
+        private System.Windows.Forms.Button btnDeporte;
+    }
+}
+
---
+++ 03CheckBox y RadioButton/03CheckBox y RadioButton/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _03CheckBox_y_RadioButton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEstado_Click(object sender, EventArgs e)
+        {
+            if (rbtSoltero.Checked == true)
+                MessageBox.Show("Ud. es " +  rbtSoltero.Text);
+            if (rbtCasado.Checked)
+                MessageBox.Show("Ud. es " + rbtCasado.Text);
+            if (rbtDivorciado.Checked)
+                MessageBox.Show("Ud. es " + rbtDivorciado.Text);
+            if (rbtViudo.Checked)
+                MessageBox.Show("Ud. es " + rbtViudo.Text);
+        }
+
+        private void btnDeporte_Click(object sender, EventArgs e)
+        {
+            if (chbFutbol.Checked==false && chbBasquet.Checked==false
+                && chbVolibol.Checked==false && chbAjedrez.Checked==false)
+            {
+                MessageBox.Show("A ud. no le gusta nada");
+            }
+            else
+            {
+                string texto = "A ud. le gusta ";
+                if (chbFutbol.Checked)
+                    texto = texto + chbFutbol.Text + " ";
+                if (chbBasquet.Checked)
+                    texto += chbBasquet.Text + " ";
+                if (chbVolibol.Checked)
+                    texto += chbVolibol.Text + " ";
+                if (chbAjedrez.Checked)
+                    texto += chbAjedrez.Text;
+                MessageBox.Show(texto);
+            }
+        }
+    }
+}
