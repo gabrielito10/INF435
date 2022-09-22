6141f4323371705818081d8bd98273743ceaef80
gabrielito10 <j.gabito@gmail.com>
2022-09-22 16:49:58 -0400

    INF435

---
+++ 09Practica/09Practica/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace _09Practica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSerie = new System.Windows.Forms.Label();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(122, 123);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(116, 23);
+            this.btnGenerar.TabIndex = 0;
+            this.btnGenerar.Text = "GENERAR";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "VALOR DE N";
+            // 
+            // lblSerie
+            // 
+            this.lblSerie.AutoSize = true;
+            this.lblSerie.Location = new System.Drawing.Point(59, 90);
+            this.lblSerie.Name = "lblSerie";
+            this.lblSerie.Size = new System.Drawing.Size(35, 13);
+            this.lblSerie.TabIndex = 2;
+            this.lblSerie.Text = "label2";
+            // 
+            // txtN
+            // 
+            this.txtN.Location = new System.Drawing.Point(138, 36);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(100, 20);
+            this.txtN.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 170);
+            this.Controls.Add(this.txtN);
+            this.Controls.Add(this.lblSerie);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGenerar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSerie;
+        private System.Windows.Forms.TextBox txtN;
+    }
+}
+
---
+++ 09Practica/09Practica/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _09Practica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            int N = int.Parse(txtN.Text);
+            string cadena = "1";
+            int serie = 1;
+            for (int i=1 ; i<N; i++)
+            {
+                if (i % 2 != 0) // es impar
+                    serie += 2;
+                else // es par
+                    serie *= 2;
+                cadena += "," + serie;
+                lblSerie.Text = cadena;
+            }
+
+        }
+    }
+}
